04b7ad34bafcbba803f03f14ae1e22586af9b8f6
David Holt <d@tdfd.org>
2020-04-19 17:52:25 -0400

    finalize symbol layer, remove transparency for unused keys

--- keyboards/kyria/keymaps/davidtedfordholt/keymap.c
+++ keyboards/kyria/keymaps/davidtedfordholt/keymap.c
@@ -39,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       KC_TAB,     KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                                     KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,     KC_MINS,
       KC_GRAVE,   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                                     KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
-      LT(_LOWER), KC_Z,  KC_X,  KC_C,  KC_V,  KC_B, _______, _______, _______, _______, KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,  KC_EQL,
-            _______,  KC_BSPC, KC_LSFT, KC_LALT, KC_LCTL, LT(_RAISE, KC_ENT), LT(_LOWER, KC_SPC), KC_SPC, KC_ENT, _______
+      LT(_LOWER), KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,        ,        ,        ,        , KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,  KC_EQL,
+            ,  KC_BSPC, KC_LSFT, KC_LALT, KC_LCTL, LT(_RAISE, KC_ENT), LT(_LOWER, KC_SPC), KC_SPC, KC_ENT, 
     ),
 /*
  * Lower Layer: Symbols
@@ -59,8 +59,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT(
       KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                     _______, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
       _______, _______, _______, _______, _______, _______,                                     KC_HOME, KC_LEFT, KC_UP,   KC_RGHT, KC_END,  _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_DOWN, KC_PGUP, _______, _______,
-                                 _______, KC_DEL,  KC_LSFT, KC_LALT, KC_LCTL, KC_ENT,  KC_SPC,  KC_SPC,  KC_ENT,  _______
+      _______, _______, _______, _______, _______, _______,        ,        ,        ,        , _______, KC_PGDN, KC_DOWN, KC_PGUP, _______, _______,
+                                        , KC_DEL,  KC_LSFT, KC_LALT, KC_LCTL, KC_ENT,  KC_SPC,  KC_SPC,  KC_ENT,  
     ),
 /*
  * Raise Layer: Number keys, media, navigation
@@ -79,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT(
       KC_ESC,   KC_1, 	 KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    ,
       RGB_MOD,  ,        RGB_SAI, RGB_HUI, RGB_VAI, RGB_M_P,                                     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
-      RGB_RMOD, RGB_TOG, RGB_SAD, RGB_HUD, RGB_VAD, RGB_M_B, _______, _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-                                  _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______
+      RGB_RMOD, RGB_TOG, RGB_SAD, RGB_HUD, RGB_VAD, RGB_M_B,        ,        ,        ,        , KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+                                         , _______, _______, _______, KC_DEL,  _______, _______, _______, _______, 
     )
 // /*
 //  * Layer template
