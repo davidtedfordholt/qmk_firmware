6c8f693d688a9e838164e1abba887b16e83a890b
David Holt <d@tdfd.org>
2020-04-19 17:53:48 -0400

    make raise layer bottom row match lower layer bottom row

--- keyboards/kyria/keymaps/davidtedfordholt/keymap.c
+++ keyboards/kyria/keymaps/davidtedfordholt/keymap.c
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,   KC_1, 	 KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    ,
       RGB_MOD,  ,        RGB_SAI, RGB_HUI, RGB_VAI, RGB_M_P,                                     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
       RGB_RMOD, RGB_TOG, RGB_SAD, RGB_HUD, RGB_VAD, RGB_M_B,        ,        ,        ,        , KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-                                         , _______, _______, _______, KC_DEL,  _______, _______, _______, _______, 
+                                        , KC_DEL,  KC_LSFT, KC_LALT, KC_LCTL, KC_ENT,  KC_SPC,  KC_SPC,  KC_ENT,  
     )
 // /*
 //  * Layer template
